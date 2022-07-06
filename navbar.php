65e2cd3cb193694c62d23172cd2c7107dbdc53eb
diniafrina <107836227+diniafrina@users.noreply.github.com>
2022-07-06 10:43:03 +0800

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,37 @@
+ <style>
+ /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+      background-color: #000000;
+    }
+
+</style>
+
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+     
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="adminmain.php">Halaman Utama</a></li>
+        <li><a href="sisa_pepejal.php">Pengurusan Pepejal</a></li>
+        <li><a href="pembersihan_awam.php">Pembersihan Awam</a></li>
+       
+        
+        
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      
+        <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav><p>
